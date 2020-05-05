c9585e5094de15d3678fc67e42bc28a06272c504
sankalph <hasija@solulab.co>
2020-05-05 20:48:20 +0530

    Final commit

---
+++ src/saga/index.js
@@ -0,0 +1,49 @@
+import { put, takeEvery,call, all, fork } from 'redux-saga/effects'
+import {GET_DATA} from "../actionTypes/index"
+import {getDataSuccess} from "../actions/index"
+import {fetchData} from "../api/api"
+
+
+
+export function* handleDataLoad() {
+    try {
+        const data = yield call(fetchData);
+        console.log("SAGA_DATA",data)
+        yield put(getDataSuccess(data));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// function* retriveData() {
+
+
+//     try {
+      
+        
+//         const response = yield fork(fetchdata);
+        
+//         console.log("DATA:", response);
+        
+       
+//            yield put(getDataSuccess(response));
+        
+    
+      
+//       } catch (error) {
+//         console.log("TCL: function*getBlogList -> error", error);
+    
+//       }
+// }
+
+function* watchGettingData() {
+  yield takeEvery(GET_DATA, handleDataLoad)
+}
+
+
+export default function* rootSaga() {
+  yield all([
+    watchGettingData()
+  ])
+}
